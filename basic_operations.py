2cf7521ea2fd5b26fa5688016a5fac2875d1656c
Sacre Tshibanda <122829214+sacre711@users.noreply.github.com>
2025-06-19 13:47:26 +0200

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,2 +1,12 @@
-number1 = 5
-number2 = 10
+number1 = 10 # value assigned to the first variable
+number2 = 5 # value assigned to the second variable
+addition = number1 + number2
+subtraction = number1 - number2
+quotient = number1 * number2
+
+# Addition of 10 and 5 is 15
+# Subtraction of 10 and 5 is 5
+# Multiplication of 10 and 5 is 50 
+print("Addition of ", number1, " and ", number2, " is ", addition)
+print("Subtraction of ", number1, " and ", number2, " is ", subtraction)
+print("Multiplication of ", number1, " and ", number2, " is ", quotient)
