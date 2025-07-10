11ea0310ff359ef8fb45de4781f58e31e65ec857
Sacre Tshibanda <122829214+sacre711@users.noreply.github.com>
2025-07-11 01:59:40 +0200

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,1 +1,8 @@
-
+#pattern_drawing.py
+number = int(input("Enter the size of the pattern:"))
+count = 0
+ while count !== number:
+   for i in range(1, number):
+     print("*", end="") 
+     count = count + 1
+ count = 0
