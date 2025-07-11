fc6f4d180f61391feb6ca74ad19241d23cb3e020
Sacre Tshibanda <122829214+sacre711@users.noreply.github.com>
2025-07-11 05:14:53 +0200

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,11 +1,13 @@
 #temp_conversion_tool.py
-global FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-global CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius (fahrenheit):
+  global FAHRENHEIT_TO_CELSIUS_FACTOR
   return fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
   
 def convert_to_fahrenheit(celsius):
+  global CELSIUS_TO_FAHRENHEIT_FACTOR
   return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
 
 try:
