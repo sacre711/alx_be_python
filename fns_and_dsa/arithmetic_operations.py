28982dffe5f784997f84625162ea1227cd696359
Sacre Tshibanda <122829214+sacre711@users.noreply.github.com>
2025-07-11 03:57:29 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,17 @@
 #arithmetic_operations.py
 #define the function
 
-def perform_operation:
+def perform_operation(operation, float(num1), float(num2)):
+    match operation:
+      case "add":
+        result = num1+num2
+      case "subtract":
+        result = num1+num2
+      case "multiply":
+        result = num1+num2
+      case "divide":
+        if num2 == 0:
+          print("Cannot divide by 0")
+        else:
+          result = num1+num2
   
